785caa590973afe6844479b30e933b6b838dce69
monster5464 <mengseang424@gmail.com>
2025-11-10 14:24:13 +0700

    Save local changes before pulling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Navbar from "./components/Navbar";
 import Home from "./Pages/Home";
 import "./app.css";
+import  Contact  from "./Pages/Contact";
+
+
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Home/>
+      <Contact/>
     </>
   );
 }
