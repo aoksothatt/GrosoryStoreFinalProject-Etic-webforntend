484341018787263a487f382d5c99cedbc4ebf1bb
monster5464 <mengseang424@gmail.com>
2025-11-10 14:54:37 +0700

    updata

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Contact = () => {
+  return (
+    <div  className="w-full sm:w-[95%] lg:w-[90%] mx-auto px-4 sm:px-0">
+      <div  className=" justify-center items-center gap-4 sm:gap-8 md:gap-16 lg:gap-24 xl:gap-32
+          h-[56vh] sm:h-[60vh] md:h-[55vh] lg:h-[40vh] w-full 
+          bg-gradient-to-r from-blue-300 to-green-300 
+          mt-4 rounded-2xl " >
+         <h4 className='font-black pl-12  p-5 text-[#000000]'>CONTACT US</h4>
+         <h3>Contact us to easily order any food or juice and get special some promotions. We’ll get back to you quickly. </h3>
+      </div>
+    </div>
+  )
+}
+export default Contact
