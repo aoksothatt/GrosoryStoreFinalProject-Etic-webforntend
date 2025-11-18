80aa2ba981ac6a32b0cd733de67e770f17999222
monster5464 <mengseang424@gmail.com>
2025-11-18 09:40:56 +0700

    resolved this message

---
+++ src/Pages/Contact2.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Contact2 = () => {
+  return (
+    <div className='w-full h-auto justify-items-center justify-content-center'>
+        <div className='w-[300px] h-[350px]'>
+           <h3>Get in Touch</h3>
+           <p>We'd love to hear you.Fill out the forms, and we'll get back to you as soon as possible.</p>
+           <label htmlFor="">Name</label>
+           <input type="text" name="" id="" placeholder=' Your Name'/>
+           <label htmlFor="">Email</label>
+           <input type="text" name="" id="" placeholder=' Your Email'/>
+           <label htmlFor="">Subject</label>
+           <input type="text" name="" id="" placeholder=' Subject'/>
+           <label htmlFor="">Message</label>
+           <input type="text" name="" id="" placeholder='Your message'/>
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Contact2
